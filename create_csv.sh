ebbab8253cfea3ee954b7771a32870319aa4fa47
jh11g20 <jh11g20@soton.ac.uk>
2022-03-15 02:19:42 +0000

    Script now cleans data further by removing undesired scales

--- create_csv.sh
+++ create_csv.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 filename=$1
 
-content=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' |  awk 'NF')
+content=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' | sed 's#Storm Location:##'  | sed 's#Min Sea Level Pressure:##'  | sed 's#Maxium Intensity:##'  | sed 's/\;.*//'  | awk 'NF')
+StormLocationData=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' | grep 'Storm Location:' -A 1 --no-group-separator  | sed 's#Storm Location:##'  | awk 'NF')
 echo "$content"
+
+
 
