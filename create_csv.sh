346823652a0eca49cc9c4f537948699fb7282ae3
jh11g20 <jh11g20@soton.ac.uk>
2022-03-15 01:18:48 +0000

    Made script put trimmed content into variable

--- create_csv.sh
+++ create_csv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 filename=$1
 
-cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' |  awk 'NF'
+content=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' |  awk 'NF')
+echo "$content"
 
