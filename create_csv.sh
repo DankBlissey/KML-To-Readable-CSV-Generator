d9e894e62b11a2cbeef80f7d17ea5f237be5c5e5
jh11g20 <jh11g20@soton.ac.uk>
2022-03-15 00:44:34 +0000

    create_csv.sh has script to trim excess metadata down to titles and values

---
+++ create_csv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+filename=$1
+
+cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' |  awk 'NF'
+
