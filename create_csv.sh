bb33d9f9e80c9b217154cc687ab0728cea1086e0
jh11g20 <jh11g20@soton.ac.uk>
2022-03-15 04:48:36 +0000

    Code now writes to a file specified in the desired format

--- create_csv.sh
+++ create_csv.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 filename=$1
+outputname=$2
 
 content=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' | sed 's#Storm Location:##'  | sed 's#Min Sea Level Pressure:##'  | sed 's#Maxium Intensity:##'  | sed 's/\;.*//' | sed 's#.*(.*##' | awk 'NF' | xargs | sed 's#knots #knots\n#g')
-StormLocationData=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' | grep 'Storm Location:' -A 1 --no-group-separator  | sed 's#Storm Location:##'  | awk 'NF')
-
-echo "Timestamp,Latitude,Longitude,MinSeaLevelPressure,MaxIntensity"
+echo "Timestamp,Latitude,Longitude,MinSeaLevelPressure,MaxIntensity" > $outputname
 echo "$content" > MidWayFile.txt
 input="MidwayFile.txt"
 while IFS= read -r line
 do
 	echo "$line"
-done < "$input" | awk '{$4=$4","}1' | awk '{$9=$9","}1' | sed 's#, #,#g'
+done < "$input" | awk '{$4=$4","}1' | awk '{$9=$9","}1' | sed 's#, #,#g' >> $outputname
 
