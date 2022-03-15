79d0dd7c42583ac7dcc3651e5aac5a3ce392ee1f
jh11g20 <jh11g20@soton.ac.uk>
2022-03-15 14:28:28 +0000

    Added code comments

--- create_csv.sh
+++ create_csv.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
+#sets variables for the file arguments to read and write
 filename=$1
 outputname=$2
 
+#takes file contents, greps only the relevant data area then trims all the extra metadata and kml formatting as well as the titles declaring what each value is. Then adds newlines at the end of knots so that each line of data is defined
 content=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' | sed 's#Storm Location:##'  | sed 's#Min Sea Level Pressure:##'  | sed 's#Maxium Intensity:##'  | sed 's/\;.*//' | sed 's#.*(.*##' | awk 'NF' | xargs | sed 's#knots #knots\n#g')
+
+#outputs to the file the first line of the collumn names
 echo "Timestamp,Latitude,Longitude,MinSeaLevelPressure,MaxIntensity" > $outputname
+
+#writes content to a text file so that the while loop functions as it needs a file to work with
 echo "$content" > MidWayFile.txt
+
+#while loop reads each line of content and writes it to the file, then pipes reformat the commas in the right places and removes the spaces at the end of the commas, finally output is written to the output file without overwriting the collumn names from before
 input="MidwayFile.txt"
 while IFS= read -r line
 do
