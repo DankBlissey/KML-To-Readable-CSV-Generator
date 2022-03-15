c30e7bffaa50739c99abbaa914ab904e34c818ac
jh11g20 <jh11g20@soton.ac.uk>
2022-03-15 04:27:28 +0000

    Script now formats the data in the right arrangement

--- create_csv.sh
+++ create_csv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 filename=$1
 
-content=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' | sed 's#Storm Location:##'  | sed 's#Min Sea Level Pressure:##'  | sed 's#Maxium Intensity:##'  | sed 's/\;.*//' | sed 's#.*(.*##' | awk 'NF' | sed 's# #\n#')
+content=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' | sed 's#Storm Location:##'  | sed 's#Min Sea Level Pressure:##'  | sed 's#Maxium Intensity:##'  | sed 's/\;.*//' | sed 's#.*(.*##' | awk 'NF' | xargs | sed 's#knots #knots\n#g')
 StormLocationData=$(cat $filename | grep '<tr><td>' | sed 's#<tr><td>##' | sed 's#</td></tr>##' | sed 's#</table>##' | sed 's#<hr>##' | sed 's#<B>##' | sed 's#</B>##' | sed 's#]]>##' | grep 'Storm Location:' -A 1 --no-group-separator  | sed 's#Storm Location:##'  | awk 'NF')
 
-echo "$content"
-
-for line in $content
+echo "$content" > MidWayFile.txt
+input="MidwayFile.txt"
+while IFS= read -r line
 do
 	echo "$line"
-done
+done < "$input"
 
